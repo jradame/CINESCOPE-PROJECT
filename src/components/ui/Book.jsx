c26003e04ed7cde9b43bdc3c131355a23cb52cb8
justin <jradame@gmail.com>
2025-06-24 21:04:30 -0500

    lib-react-tut

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,53 +1,33 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-
-function renderStars(rating) {
-  const stars = [];
-  const fullStars = Math.floor(rating);
-  const halfStar = rating % 1 >= 0.5;
-  for (let i = 0; i < fullStars; i++) {
-    stars.push(<FontAwesomeIcon icon="star" key={i} />);
-  }
-  if (halfStar) {
-    stars.push(<FontAwesomeIcon icon="star-half-alt" key="half" />);
-  }
-  while (stars.length < 5) {
-    stars.push(<FontAwesomeIcon icon="star-o" key={`empty-${stars.length}`} />);
-  }
-  return stars;
-}
-
-const Book = ({ book }) => {
+const Book = () => {
   return (
     <div className="book">
-      <a href={book.link || '#'}>
+      <a href="">
         <figure className="book__img--wrapper">
           <img
-            src={book.image}
-            alt={book.title}
+            src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg"
+            alt=""
             className="book__img"
           />
         </figure>
       </a>
       <div className="book__title">
-        <a href={book.link || '#'} className="book__title--link">
-          {book.title}
+        <a href="" className="book__title--link">
+          Crack the Coding Interview
         </a>
       </div>
       <div className="book__ratings">
-        {renderStars(book.rating)}
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star-half-alt" />
       </div>
       <div className="book__price">
-        {book.salePrice
-          ? (
-            <>
-              <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>
-              ${book.salePrice.toFixed(2)}
-            </>
-          )
-          : <>${book.originalPrice.toFixed(2)}</>
-        }
+        <span className="book__price--normal">$15.00</span>
+        $10.00
       </div>
     </div>
   );
