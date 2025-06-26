20b14a021196846732cc07505936614d55e1c0ce
justin <jradame@gmail.com>
2025-06-26 12:33:11 -0500

    lib-react-tut

--- src/App.js
+++ src/App.js
@@ -1,49 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-import Explore from "./components/Explore";
-import Discounted from "./components/Discounted";
-import Landing from "./components/Landing";
+import Home from "./pages/Home";
 import Nav from "./components/Nav";
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
 
 
-
-
-
-function Home() {
-  return (
-    <>
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </>
-  );
-}
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div classNmae="App">
+        <Route />
         <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books books={books} />} />
-          <Route
-            path="/books/:id"
-            element={<BookInfo books={books} addToCart={addToCart} cart={cart} />}
-          />
-          <Route
-            path="/cart"
-            element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} />}
-          />
-        </Routes>
+        <Footer />
       </div>
+
     </Router>
+    
   );
 }
+
+
 
 export default App;
