d3d2ce3b4b8a97d050ed8d8d8f7299ebafa321a5
justin <jradame@gmail.com>
2025-06-26 12:09:41 -0500

    lib-react-tut

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-// import './App.css';
-import Footer from './components/Footer';
-import Explore from './components/Explore';
-import Discounted from './components/Discounted';
-import Landing from './components/Landing';
-import Nav from './components/Nav';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Explore from "./components/Explore";
+import Discounted from "./components/Discounted";
+import Landing from "./components/Landing";
+import Nav from "./components/Nav";
+import Highlights from "./components/Highlights";
+import Featured from "./components/Featured";
 
 
-function App() {
+
+
+
+function Home() {
   return (
-    <div className="App">
-      <Nav />
+    <>
       <Landing />
       <Highlights />
       <Featured />
       <Discounted />
       <Explore />
       <Footer />
-    </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={<BookInfo books={books} addToCart={addToCart} cart={cart} />}
+          />
+          <Route
+            path="/cart"
+            element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
 
 const Nav = () => {
+  function openMenu() {
+    document.body.classList.add("menu--open");
+  }
+  function closeMenu() {
+    document.body.classList.remove("menu--open");
+  }
   return (
     <nav>
       <div className="nav__container">
@@ -20,20 +26,19 @@ const Nav = () => {
               Books
             </a>
           </li>
-    
-        <button className="btn__menu">
-          <FontAwesomeIcon icon="bars" size="lg" />
-        </button>
-        <li className="nav__icon">
-          <a href="/cart" className="nav__link">
-            <FontAwesomeIcon icon="shopping-cart" />
-          </a>
-          <span className="cart__length">3</span>  
-        </li>
+          <button className="btn__menu" onClick={openMenu}>
+            <FontAwesomeIcon icon="bars" size="lg" />
+          </button>
+          <li className="nav__icon">
+            <a href="/cart" className="nav__link">
+              <FontAwesomeIcon icon="shopping-cart" />
+            </a>
+            <span className="cart__length">3</span>
+          </li>
         </ul>
         <div className="menu__backdrop">
-          <button className="btn__menu btn__menu--close">
-            <FontAwesomeIcon icon="times" /> 
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
+            <FontAwesomeIcon icon="times" />
           </button>
           <ul className="menu__links">
             <li className="menu__list">
@@ -51,8 +56,7 @@ const Nav = () => {
                 Cart
               </a>
             </li>
-          </ul>   
-
+          </ul>
         </div>
       </div>
     </nav>
